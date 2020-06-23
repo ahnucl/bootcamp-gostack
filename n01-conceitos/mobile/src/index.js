db32eb38da423ff01c4ddf0321a75162b5a0ba2e
Leonardo Cunha <lalmcunha@gmail.com>
2020-06-23 14:57:17 -0300

    listando projetos ok

--- n01-conceitos/mobile/src/index.js
+++ n01-conceitos/mobile/src/index.js
@@ -1,10 +1,49 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { SafeAreaView, Text, StyleSheet, StatusBar, FlatList } from 'react-native';
+
+import api from './services/api';
 
 export default function App() {
+    const [ projects, setProjects ] = useState([]);
+
+    useEffect( () => {
+        api.get('projects').then( response => {
+
+            setProjects(response.data);
+        });
+    }, []);
+
     return ( 
-        <View>
-            <Text> Olá mundo!!</Text>
-        </View>
+        <>
+            <StatusBar barStyle="light-content" backgroundColor='#7159c1' />
+            {/* <View style={styles.container}>
+                {projects.map( project => (
+                    <Text style={styles.projectList} key={project.id}>{project.title}</Text>
+                ))}
+            </View> */}
+            <SafeAreaView style={styles.container}>
+                <FlatList
+                    data={projects}
+                    keyExtractor={ project => project.id }
+                    renderItem={ ({ item: project }) => (
+                        <Text style={styles.projectList}>{project.title}</Text>
+                    )}
+                />
+            </SafeAreaView>
+
+        </>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#7159c1',
+        // justifyContent: "center",
+        // alignItems: "center"
+    },
+    projectList: {
+        color: '#fdfdfd',
+        fontSize: 32,
+    }
+});
