a4762c987b51a45b0442caf6e1b656a8e24e58f1
Leonardo Cunha <lalmcunha@gmail.com>
2020-06-25 23:13:24 -0300

    eslint configurado para projeto node

--- n02-primeiro-projeto-node/src/server.ts
+++ n02-primeiro-projeto-node/src/server.ts
@@ -1,12 +1,11 @@
 import express from 'express';
+import routes from './routes';
 
 const app = express();
 
-app.get('/', (request, response) => {
-    const a = 'q23';
-    return response.json({message: 'Get route'});
-});
+app.get('/', (request, response) => response.json({ message: 'Get route' }));
 
-app.listen(3333, () =>{
-    console.log('Server started on port 3333');
+app.listen(3333, () => {
+  // eslint-disable-next-line no-console
+  console.log('Server started on port 3333');
 });
