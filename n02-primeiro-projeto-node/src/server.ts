76ab0446cab4e03e3f03331c37c4c5b20beae64e
Leonardo Cunha <lalmcunha@gmail.com>
2020-06-29 13:28:34 -0300

    Validação de datas com interface

--- n02-primeiro-projeto-node/src/server.ts
+++ n02-primeiro-projeto-node/src/server.ts
@@ -8,5 +8,5 @@ app.use(routes);
 
 app.listen(3333, () => {
   // eslint-disable-next-line no-console
-  console.log('Server started on port 3333');
+  console.log('...Server started on port 3333');
 });
