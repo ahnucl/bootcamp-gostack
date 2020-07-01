d45dce477e87f106b1c62a811151559b1acaa7aa
Leonardo Cunha <lalmcunha@gmail.com>
2020-07-01 20:37:19 -0300

    Serviço de autenticação ok

---
+++ n02-02-iniciando-back-end/src/routes/sessions.routes.ts
@@ -0,0 +1,26 @@
+import { Router } from 'express';
+
+import AuthenticateUserService from '../services/AnthenticateUserService';
+
+const sessionsRouter = Router();
+
+sessionsRouter.post('/', async (request, response) => {
+  try {
+    const { password, email } = request.body;
+
+    const authenticateUser = new AuthenticateUserService();
+
+    const { user } = await authenticateUser.execute({
+      email,
+      password,
+    });
+
+    delete user.password;
+
+    return response.json({ user });
+  } catch (err) {
+    return response.status(400).json({ error: err.message });
+  }
+});
+
+export default sessionsRouter;
