f49da079973c24e25593e5256980025815995718
Leonardo Cunha <lalmcunha@gmail.com>
2020-06-30 23:56:34 -0300

    ajsutes na rota e no service

--- n02-02-iniciando-back-end/src/routes/appointments.routes.ts
+++ n02-02-iniciando-back-end/src/routes/appointments.routes.ts
@@ -1,32 +1,32 @@
 import { parseISO } from 'date-fns';
 import { Router } from 'express';
+import { getCustomRepository } from 'typeorm';
 import AppointmentsRepository from '../repositories/AppointmentsRepository';
 import CreateAppointmentService from '../services/CreateAppointmentService';
 
 const appointmentRouter = Router();
-const appointmentsRepository = new AppointmentsRepository();
 
 appointmentRouter.get('/', (request, response) => {
-  const appointments = appointmentsRepository.all();
+  const appointmentsRepository = getCustomRepository(AppointmentsRepository);
+  const appointments = appointmentsRepository.find();
 
   return response.json(appointments);
 });
 
-appointmentRouter.post('/', (request, response) => {
-  const { provider, date } = request.body;
-
-  const parsedDate = parseISO(date); // transformação de dados
-  // const appointmentDate = startOfHour(parsedDate); // regra de negócio
-
-  const createAppointment = new CreateAppointmentService(
-    appointmentsRepository,
-  );
-
+appointmentRouter.post('/', async (request, response) => {
   try {
-    const appointment = createAppointment.execute({
+    const { provider, date } = request.body;
+
+    const parsedDate = parseISO(date); // transformação de dados
+    // const appointmentDate = startOfHour(parsedDate); // regra de negócio
+
+    const createAppointment = new CreateAppointmentService();
+
+    const appointment = await createAppointment.execute({
       date: parsedDate,
       provider,
     });
+
     return response.json(appointment);
   } catch (err) {
     return response.status(400).json({ error: err.message });
