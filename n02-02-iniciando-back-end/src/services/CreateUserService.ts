24bf1ca0a38104a5049cbc0a8e45d42ad019cd5e
Leonardo Cunha <lalmcunha@gmail.com>
2020-07-01 18:41:41 -0300

    userCreateService ok, tipo User.email corrigido

--- n02-02-iniciando-back-end/src/services/CreateUserService.ts
+++ n02-02-iniciando-back-end/src/services/CreateUserService.ts
@@ -9,6 +9,24 @@ interface Request {
 class CreateUserService {
   public async execute({ name, email, password }: Request): Promise<User> {
     const userRepository = getRepository(User);
+
+    const checkEmailIsUsed = await userRepository.findOne({
+      where: { email },
+    });
+
+    if (checkEmailIsUsed) {
+      throw new Error('Email address already in use.');
+    }
+
+    const user = userRepository.create({
+      name,
+      email,
+      password,
+    });
+
+    await userRepository.save(user);
+
+    return user;
   }
 }
 
