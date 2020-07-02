f2b985b024cdff6acb00fbbfb11c4c3e1408ab30
Leonardo Cunha <lalmcunha@gmail.com>
2020-07-02 17:09:24 -0300

    Criado middleware para tratamento de exceções

--- n02-02-iniciando-back-end/src/routes/sessions.routes.ts
+++ n02-02-iniciando-back-end/src/routes/sessions.routes.ts
@@ -5,22 +5,18 @@ import AuthenticateUserService from '../services/AnthenticateUserService';
 const sessionsRouter = Router();
 
 sessionsRouter.post('/', async (request, response) => {
-  try {
-    const { password, email } = request.body;
+  const { password, email } = request.body;
 
-    const authenticateUser = new AuthenticateUserService();
+  const authenticateUser = new AuthenticateUserService();
 
-    const { user, token } = await authenticateUser.execute({
-      email,
-      password,
-    });
+  const { user, token } = await authenticateUser.execute({
+    email,
+    password,
+  });
 
-    delete user.password;
+  delete user.password;
 
-    return response.json({ user, token });
-  } catch (err) {
-    return response.status(err.statusCode).json({ error: err.message });
-  }
+  return response.json({ user, token });
 });
 
 export default sessionsRouter;
