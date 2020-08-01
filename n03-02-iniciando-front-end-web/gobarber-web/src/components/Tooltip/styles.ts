6776972a5dccc4db8b2f3922e1fc9fdf5124b810
Leonardo Cunha <lalmcunha@gmail.com>
2020-07-31 22:33:48 -0300

    Corrigindo fade out do tooltip

--- n03-02-iniciando-front-end-web/gobarber-web/src/components/Tooltip/styles.ts
+++ n03-02-iniciando-front-end-web/gobarber-web/src/components/Tooltip/styles.ts
@@ -11,7 +11,7 @@ export const Container = styled.div`
     font-size: 14px;
     font-weight: 500;
     opacity: 0;
-    transition: opacity 0.4s;
+    transition: opacity 0.4s, visibility 0.4s;
     visibility: hidden;
 
     position: absolute;
