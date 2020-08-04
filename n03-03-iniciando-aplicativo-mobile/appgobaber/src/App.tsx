3f90e2b1b2c9a4dfab363c81a9b9d84cd7fb7b31
Leonardo Cunha <lalmcunha@gmail.com>
2020-08-03 21:26:32 -0300

    Configuração de padrão ok

--- n03-03-iniciando-aplicativo-mobile/appgobaber/src/App.tsx
+++ n03-03-iniciando-aplicativo-mobile/appgobaber/src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import { View, Text } from 'react-native';
 
-const App: React.FC = () => (
-  <View>
-    <Text>Hello, Native!</Text>
-  </View>
-);
+const App: React.FC = () => {
+  return (
+    <View>
+      <Text>Hello, Native!</Text>
+    </View>
+  );
+};
 
 export default App;
