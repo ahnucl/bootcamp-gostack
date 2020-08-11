e30ccb5078cff11275131874cf5b799afde93256
Leonardo Cunha <lalmcunha@gmail.com>
2020-08-11 19:50:33 -0300

    Removido "width" do Button para corrigir

--- n03-03-iniciando-aplicativo-mobile/appgobaber/src/pages/SignIn/index.tsx
+++ n03-03-iniciando-aplicativo-mobile/appgobaber/src/pages/SignIn/index.tsx
@@ -54,11 +54,11 @@ const SignIn: React.FC = () => {
             <Form ref={formRef} onSubmit={handleSignIn}>
               <Input name="email" icon="mail" placeholder="E-mail" />
               <Input name="password" icon="lock" placeholder="Senha" />
-            </Form>
 
-            <Button onPress={() => formRef.current?.submitForm()}>
-              Entrar
-            </Button>
+              <Button onPress={() => formRef.current?.submitForm()}>
+                Entrar
+              </Button>
+            </Form>
 
             <ForgotPassword onPress={() => {}}>
               <ForgotPasswordText>Esqueci minha senha</ForgotPasswordText>
