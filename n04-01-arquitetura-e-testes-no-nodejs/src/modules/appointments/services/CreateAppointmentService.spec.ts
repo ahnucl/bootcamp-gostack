81376baeb00de7138196c69313cc8628f4013376
Leonardo Cunha <lalmcunha@gmail.com>
2020-08-20 21:01:30 -0300

    jest configurado para coverage

--- n04-01-arquitetura-e-testes-no-nodejs/src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ n04-01-arquitetura-e-testes-no-nodejs/src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -1,7 +1,21 @@
+import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentsRepository';
 import CreateAppointmentService from './CreateAppointmentService';
 
 describe('CreateAppointmentService', () => {
-  it('should be able to create a new appointment', () => {});
+  it('should be able to create a new appointment', async () => {
+    const fakeAppointmentsRepository = new FakeAppointmentsRepository();
+    const createAppointmentService = new CreateAppointmentService(
+      fakeAppointmentsRepository,
+    );
+
+    const appointment = await createAppointmentService.execute({
+      date: new Date(),
+      provider_id: '12345',
+    });
+
+    expect(appointment).toHaveProperty('id');
+    expect(appointment.provider_id).toBe('12345');
+  });
 
   // it('should not be able to create two appointments on the same time', () => {
 
