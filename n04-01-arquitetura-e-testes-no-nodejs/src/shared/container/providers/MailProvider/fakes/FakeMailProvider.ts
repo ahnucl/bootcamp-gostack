ac047f5a257f9a12863c1e3fd515875bc4d97c0c
Leonardo Cunha <lalmcunha@gmail.com>
2020-08-31 22:19:59 -0300

    corrigindo UnhadledPromiseRejection nos testes

--- n04-01-arquitetura-e-testes-no-nodejs/src/shared/container/providers/MailProvider/fakes/FakeMailProvider.ts
+++ n04-01-arquitetura-e-testes-no-nodejs/src/shared/container/providers/MailProvider/fakes/FakeMailProvider.ts
@@ -6,7 +6,7 @@ interface IMessage {
 }
 
 export default class FakeMailProvider implements IMailProvider {
-  private messages: IMessage[];
+  private messages: IMessage[] = [];
 
   public async sendMail(to: string, body: string): Promise<void> {
     this.messages.push({ to, body });
