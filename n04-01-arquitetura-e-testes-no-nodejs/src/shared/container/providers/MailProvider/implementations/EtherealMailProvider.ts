49f99f699e7c44cde16ce42cdfd40bef5529c208
Leonardo Cunha <lalmcunha@gmail.com>
2020-08-31 23:23:30 -0300

    Recuperação e reset de senha em DEV ok

---
+++ n04-01-arquitetura-e-testes-no-nodejs/src/shared/container/providers/MailProvider/implementations/EtherealMailProvider.ts
@@ -0,0 +1,44 @@
+/* eslint-disable no-console */
+import nodemailer, { Transporter } from 'nodemailer';
+import IMailProvider from '../models/IMailProvider';
+
+export default class EtherealMailProvider implements IMailProvider {
+  private client: Transporter;
+
+  constructor() {
+    nodemailer.createTestAccount((err, account) => {
+      if (err) {
+        console.error(
+          `#EtherealMailProvider: Não foi possível criar conta de teste ${err.message}`,
+        );
+        return process.exit(1);
+      }
+
+      const transporter = nodemailer.createTransport({
+        host: account.smtp.host,
+        port: account.smtp.port,
+        secure: account.smtp.secure,
+        auth: {
+          user: account.user,
+          pass: account.pass,
+        },
+      });
+
+      console.log(account);
+
+      this.client = transporter;
+    });
+  }
+
+  public async sendMail(to: string, body: string): Promise<void> {
+    const message = await this.client.sendMail({
+      from: 'Equipe GoBarber <equipe@gobarber.com.br>',
+      to,
+      subject: 'Recuperação de senha',
+      text: body,
+    });
+
+    console.log('Message sent: %s', message.messageId);
+    console.log('Preview URL: %s', nodemailer.getTestMessageUrl(message));
+  }
+}
