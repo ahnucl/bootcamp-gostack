66ca52cc5f71b958903e35403f9ef6a3b8c66503
Leonardo Cunha <lalmcunha@gmail.com>
2020-09-05 17:19:23 -0300

    Últimas regras de negócio de appointments implementadas

--- n04-01-arquitetura-e-testes-no-nodejs/src/modules/appointments/services/CreateAppointmentService.ts
+++ n04-01-arquitetura-e-testes-no-nodejs/src/modules/appointments/services/CreateAppointmentService.ts
@@ -1,4 +1,4 @@
-import { startOfHour } from 'date-fns';
+import { startOfHour, isBefore, getHours } from 'date-fns';
 import { injectable, inject } from 'tsyringe';
 
 import AppError from '@shared/errors/AppError';
@@ -26,11 +26,29 @@ class CreateAppointmentService {
   }: IRequest): Promise<Appointment> {
     const appointmentDate = startOfHour(date); // regra de negócio
 
+    if (isBefore(appointmentDate, Date.now())) {
+      console.log("You can't create an apointment on a passed date.");
+      throw new AppError("You can't create an apointment on a passed date.");
+    }
+
+    if (user_id === provider_id) {
+      console.log("You can't create an appointment with yourself.");
+      throw new AppError("You can't create an appointment with yourself.");
+    }
+
+    if (getHours(appointmentDate) < 8 || getHours(appointmentDate) > 17) {
+      console.log('You can only create appointments between 8 am and 5 pm.');
+      throw new AppError(
+        'You can only create appointments between 8 am and 5 pm.',
+      );
+    }
+
     const findAppointmentInSameDate = await this.appointmentsRepository.findByDate(
       appointmentDate,
     );
 
     if (findAppointmentInSameDate) {
+      console.log('This appointment is already booked.');
       throw new AppError('This appointment is already booked.');
     }
 
