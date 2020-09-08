475baf39e7440f2818f481869be094f41749b7c6
Leonardo Cunha <lalmcunha@gmail.com>
2020-09-08 20:14:50 -0300

    Instalando celebrate e validando rotas

--- n04-03-finalizando-back-end-do-app/src/modules/users/infra/http/routes/users.routes.ts
+++ n04-03-finalizando-back-end-do-app/src/modules/users/infra/http/routes/users.routes.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import multer from 'multer';
 import uploadConfig from '@config/upload';
+import { celebrate, Segments, Joi } from 'celebrate';
 
 import UsersController from '@modules/users/infra/http/controllers/UsersController';
 import UserAvatarContoller from '@modules/users/infra/http/controllers/UserAvatarController';
@@ -11,7 +12,17 @@ const upload = multer(uploadConfig);
 const usersController = new UsersController();
 const userAvatarContoller = new UserAvatarContoller();
 
-usersRouter.post('/', usersController.create);
+usersRouter.post(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+      email: Joi.string().email().required(),
+      password: Joi.string().required(),
+    },
+  }),
+  usersController.create,
+);
 
 usersRouter.patch(
   '/avatar',
