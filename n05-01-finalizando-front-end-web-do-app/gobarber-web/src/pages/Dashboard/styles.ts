7f796a1ec3085e86fc1a804acceb7e03673f67c9
Leonardo Cunha <lalmcunha@gmail.com>
2020-09-19 16:32:33 -0300

    Próximo agendamento

--- n05-01-finalizando-front-end-web-do-app/gobarber-web/src/pages/Dashboard/styles.ts
+++ n05-01-finalizando-front-end-web-do-app/gobarber-web/src/pages/Dashboard/styles.ts
@@ -75,20 +75,78 @@ export const Schedule = styled.div`
   p {
     margin-top: 8px;
     color: #ff9000;
+    display: flex;
+    align-items: center;
+    font-weight: 500;
 
     span {
-      position: relative;
+      display: flex;
+      align-items: center;
     }
 
-    span + span {
-      margin-left: 8px;
-      padding-left: 8px;
-      border-left: 1px solid #ff9000;
+    span + span::before {
+      content: '';
+      width: 1px;
+      height: 12px;
+      background: #ff9000;
+      margin: 0 8px;
     }
   }
 `;
 
-export const NextAppointment = styled.div``;
+export const NextAppointment = styled.div`
+  margin-top: 64px;
+
+  > strong {
+    color: #999591;
+    font-size: 20px;
+    font-weight: 400;
+  }
+
+  div {
+    background: #3e3b47;
+    display: flex;
+    align-items: center;
+    padding: 16px 24px;
+    border-radius: 10px;
+    margin-top: 24px;
+    position: relative;
+
+    &::before {
+      content: '';
+      position: absolute;
+      height: 80%;
+      width: 1px;
+      left: 0;
+      top: 10%;
+      background: #ff9000;
+    }
+
+    img {
+      width: 80px;
+      height: 80px;
+      border-radius: 50%;
+      object-fit: cover;
+    }
+
+    strong {
+      margin-left: 24px;
+      color: #fff;
+    }
+
+    span {
+      margin-left: auto;
+      display: flex;
+      align-items: center;
+      color: #999591;
+
+      svg {
+        color: #ff9000;
+        margin-right: 8px;
+      }
+    }
+  }
+`;
 
 export const Calendar = styled.aside`
   width: 380px;
