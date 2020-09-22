8eb34d108256b576db5f6d2aa2364b30f8f4ae38
Leonardo Cunha <lalmcunha@gmail.com>
2020-09-21 21:58:48 -0300

    Página de perfil

--- n05-01-finalizando-front-end-web-do-app/gobarber-web/src/routes/index.tsx
+++ n05-01-finalizando-front-end-web-do-app/gobarber-web/src/routes/index.tsx
@@ -8,14 +8,16 @@ import ForgotPassword from '../pages/ForgotPassword';
 import ResetPassword from '../pages/ResetPassword';
 
 import Dashboard from '../pages/Dashboard';
+import Profile from '../pages/Profile';
 
 const Routes: React.FC = () => (
   <Switch>
     <Route path="/" exact component={SignIn} />
-    <Route path="/signup" exact component={SignUp} />
-    <Route path="/forgot-password" exact component={ForgotPassword} />
-    <Route path="/reset-password" exact component={ResetPassword} />
+    <Route path="/signup" component={SignUp} />
+    <Route path="/forgot-password" component={ForgotPassword} />
+    <Route path="/reset-password" component={ResetPassword} />
 
+    <Route path="/profile" component={Profile} isPrivate />
     <Route path="/dashboard" component={Dashboard} isPrivate />
   </Switch>
 );
